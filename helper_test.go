37a143b226ecee6c5780637948b733eacefa62eb
templexxx <lucas1x1x@gmail.com>
2022-11-22 01:18:05 +0800

    mv dedup to helper_test.go

--- helper_test.go
+++ helper_test.go
@@ -143,3 +143,21 @@ func isIn(e int, s []int) bool {
 	}
 	return false
 }
+
+// dedup removes duplicates from a given slice
+func dedup(s []int) []int {
+
+	sort.Ints(s)
+
+	cnt := len(s)
+	cntDup := 0
+	for i := 1; i < cnt; i++ {
+		if s[i] == s[i-1] {
+			cntDup++
+		} else {
+			s[i-cntDup] = s[i]
+		}
+	}
+
+	return s[:cnt-cntDup]
+}
